5f9912f8ccc7d90f6481988d217f475294de924c
geekyatheist <jon@geekyatheist.com>
2010-06-12 16:53:59 -0500

    Revert "Added statistics plots"
    This reverts commit 69fcbed1b3cf977b723df8ba4168a24981521255. It accidentally deleted the skeleton project.

---
+++ src/com/reddit/worddit/WordditHome.java
@@ -0,0 +1,15 @@
+package com.reddit.worddit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordditHome extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
