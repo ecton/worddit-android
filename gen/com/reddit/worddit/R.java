2988e72f70a600c2ac78c646ec9abdfb72a10a2a
Christopher Ryan Cope <Procrastinator@Christopher-Ryan-Copes-MacBook-Pro.local>
2010-07-01 00:05:46 -0400

    Fixed apostrophe escaping issue.

--- gen/com/reddit/worddit/R.java
+++ gen/com/reddit/worddit/R.java
@@ -12,12 +12,31 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int worddit=0x7f020001;
+    }
+    public static final class id {
+        public static final int login_button_login=0x7f050007;
+        public static final int login_check_new=0x7f050008;
+        public static final int login_container_buttons=0x7f050006;
+        public static final int login_form=0x7f050002;
+        public static final int login_header_graphic=0x7f050001;
+        public static final int login_input_confirmpassword=0x7f050005;
+        public static final int login_input_email=0x7f050003;
+        public static final int login_input_password=0x7f050004;
+        public static final int login_root=0x7f050000;
+        public static final int text=0x7f050009;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int hint_email=0x7f040004;
+        public static final int hint_password=0x7f040005;
+        public static final int hint_password_confirm=0x7f040006;
+        public static final int label_create=0x7f040003;
+        public static final int label_login=0x7f040002;
         public static final int worddit_server=0x7f040001;
     }
 }
