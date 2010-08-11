baab8bbbeb236ed1576f2089d7d1d84a4118e979
Paul Kilgo [OEP] <paulkilgo+github@gmail.com>
2010-08-11 16:53:15 -0500

    Added a UI idea for the EditProfileActivity

---
+++ src/com/reddit/worddit/EditProfileActivity.java
@@ -0,0 +1,11 @@
+package com.reddit.worddit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditProfileActivity extends Activity {
+
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+	}
+}
